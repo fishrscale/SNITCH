5a8fc2c224547d723be899bd94e5a5062c2d74e0
Robin Grolaux <Robin.Grolaux@monash.edu>
2025-04-08 15:24:55 +1000

    Adj pval

--- R/parallel_processing.R
+++ R/parallel_processing.R
@@ -37,5 +37,13 @@ run_parallel_classification <- function(dat_cor, age, ages_grid, covariates = NU
   }))
   results_df$adj_lm <- p.adjust(results_df$lm_pval)
   results_df$adj_white <- p.adjust(results_df$white_pval)
+  results_df$classification <- NA
+
+  results_df[results_df$adj_lm <= 0.01 & results_df$lm_coef > 0, 'classification'] <- 'LI'
+  results_df[results_df$adj_lm <= 0.01 & results_df$lm_coef < 0, 'classification'] <- 'LD'
+  results_df[results_df$adj_lm > 0.01 & results_df$adj_white <= 0.01, 'classification'] <- 'VI'
+  results_df[results_df$adj_lm > 0.01 & results_df$adj_white > 0.01, 'classification'] <- 'NC'
+  results_df[results_df$dbic_lg > 2, 'classification'] <- 'NL'
+
   return(results_df)
 }
